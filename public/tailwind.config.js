4d5554f52e14ca43813724468936efe95fa26da4
Admin <admin@Chevys-Mac.local>
2023-07-03 23:27:59 -0400

    changes

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      cyan: 'hsl(180, 66%, 49%)',
-      cyanLight: 'hsl(180, 66%, 69%)',
-      darkViolet: 'hsl(257, 27%, 26%)',
-      red: 'hsl(0, 87%, 67%)',
-      grayishViolet: 'hsl(257, 7%, 63%)',
-      veryDarkBlue: 'hsl(255, 11%, 22%)',
-      veryDarkViolet: 'hsl(260, 8%, 14%)',
+        cyan: 'hsl(180, 66%, 49%)',
+        cyanLight: 'hsl(180, 66%, 69%)',
+        darkViolet: 'hsl(257, 27%, 26%)',
+        red: 'hsl(0, 87%, 67%)',
+        grayishViolet: 'hsl(257, 7%, 63%)',
+        veryDarkBlue: 'hsl(255, 11%, 22%)',
+        veryDarkViolet: 'hsl(260, 8%, 14%)',
       },
       fontFamily: {
         sans: ['Poppins', 'sans-serif']
@@ -21,5 +21,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+},
 
